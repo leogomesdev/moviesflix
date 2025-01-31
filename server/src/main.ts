3dd7da45c5476b37927b748a6962cc46e2432cde
Leonardo Gomes <lgomes@pelmorex.com>
2025-01-30 19:17:35 -0500

    [internal] Disable CORS

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,12 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: [process.env.ALLOW_CORS_URL],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true,
-  });
-
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
